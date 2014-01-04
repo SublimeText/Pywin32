35733546a9ff5ddb35ac635994e0120c568b3b1a
facelessuser <faceless.shop@gmail.com>
2014-01-04 08:58:22 -0700

    - Add win32com support - Remove scripts that should be included

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
 import sublime
 import sys
-from os.path import join
+from os.path import join, dirname
+import struct
 
 assert(sys.platform.startswith('win'))
 
+ARCH = "x%d" % (8 * struct.calcsize("P"))
+PACKAGE_PATH = dirname(__file__)
+LIB_PATHS = [
+    (join(PACKAGE_PATH, "lib", ARCH, "win32"), "win32"),
+    (join(PACKAGE_PATH, "lib", ARCH, "win32", "lib"), "win32/lib"),
+    (join(PACKAGE_PATH, "lib", ARCH), "win32com"),
+    (join(PACKAGE_PATH, "lib", ARCH, "win32comext"), "win32comext")
+]
 
-def win32setup():
-    if sublime.platform() == "windows":
-        try:
-            import win32api
-        except:
-            arch = sublime.arch()
-            sys.path.append(join(sublime.packages_path(), "Pywin32", "lib", arch, "win32"))
-            sys.path.append(join(sublime.packages_path(), "Pywin32", "lib", arch, "win32", "lib"))
+for lib in LIB_PATHS:
+    if lib[0] not in sys.path:
+        sys.path.append(lib[0])
+        print("Pywin32: Added '%s'" % lib[1])
